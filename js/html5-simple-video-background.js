72bc29d9e0e32f16e0d4a1f0ea86c3f19def3cc6
Ci Tidd <contact@ctidd.com>
2013-08-10 13:54:19 -0500

    Add script

---
+++ js/html5-simple-video-background.js
@@ -0,0 +1,39 @@
+//
+// HTML5 Simple Video Background
+// github.com/ctidd/HTML5-Simple-Video-Background
+//
+
+
+$(document).ready(function() {
+
+    $(window).load(function() {
+        resizeVideoBackground();
+    });
+
+    $(window).resize(function() {
+        resizeVideoBackground();
+    });
+
+});
+
+function resizeVideoBackground() {
+
+    var backgroundHeight = $('.video-background video').height();
+    var backgroundWidth = $('.video-background video').width();
+
+    var htmlHeight = $('html').height();
+    var htmlWidth = $('html').width();
+
+    if (backgroundHeight > htmlHeight) {
+        $('.video-background video').css('margin-top', '-' + (backgroundHeight-htmlHeight)/2 + 'px');
+    } else {
+        $('.video-background video').css('margin-top', '0');
+    }
+
+    if (backgroundWidth > htmlWidth) {
+        $('.video-background video').css('margin-left', '-' + (backgroundWidth-htmlWidth)/2 + 'px');
+    } else {
+        $('.video-background video').css('margin-left', '0');
+    }
+
+}
